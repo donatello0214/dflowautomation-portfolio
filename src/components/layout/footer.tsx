11daae4b5248d8143256927851c99ff6f320b557
Don Sufrir <donatello0214@gmail.com>
2025-11-04 10:28:56 +0000

    Make those letters in bold, and in white color

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -13,7 +13,7 @@ export function Footer() {
               FlowAutomation
             </span>
           </span>
-          <p className="text-xs text-primary-foreground/60">Automate. Integrate. Elevate.</p>
+          <p className="text-xs font-bold text-white">Automate. Integrate. Elevate.</p>
         </div>
         <p className="text-center text-sm">
           <span className="text-primary-foreground/60 dark:text-white">© {new Date().getFullYear()} </span>
