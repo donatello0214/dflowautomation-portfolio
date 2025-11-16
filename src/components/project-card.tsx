7d8935d325532f84d0b8a3f93c8e5c38d3f3812d
Don Sufrir <donatello0214@gmail.com>
2025-11-16 10:07:15 +0000

    make it 5x larger

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -197,7 +197,7 @@ export function ProjectCard({ project }: { project: Project }) {
             </button>
             
             <div 
-              className="relative h-[95vh] w-[95vw] flex items-center justify-center"
+              className="relative h-screen w-screen flex items-center justify-center p-4"
               onClick={(e) => e.stopPropagation()}
             >
               <Image
