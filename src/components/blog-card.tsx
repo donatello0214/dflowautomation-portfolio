5c1171062b8566ea8c4b66fcc3c1264c8e4bad02
Don Sufrir <donatello0214@gmail.com>
2025-11-16 14:10:57 +0000

    still fail, first put a physical scrollbar at the right side of the moda

--- src/components/blog-card.tsx
+++ src/components/blog-card.tsx
@@ -64,7 +64,7 @@ export function BlogCard({ article }: { article: BlogArticle }) {
             Published on {article.date}
           </DialogDescription>
         </DialogHeader>
-        <ScrollArea className="flex-1 pr-6">
+        <ScrollArea className="flex-1 overflow-y-scroll pr-6">
           <div
             className="prose prose-sm dark:prose-invert max-w-none py-4 text-foreground/80"
             dangerouslySetInnerHTML={{ __html: article.content }}
