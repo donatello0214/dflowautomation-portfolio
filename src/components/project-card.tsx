9c87c000c9da320a2c5597c40a1b45e56630662e
Don Sufrir <donatello0214@gmail.com>
2025-11-16 09:56:54 +0000

    your dumb as fuck, it is still the same zoom size

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -197,7 +197,7 @@ export function ProjectCard({ project }: { project: Project }) {
             </button>
             
             <div 
-              className="relative w-[95vw] h-[95vh] flex items-center justify-center"
+              className="relative h-[95vh] w-[95vw] flex items-center justify-center"
               onClick={(e) => e.stopPropagation()}
             >
               <Image
